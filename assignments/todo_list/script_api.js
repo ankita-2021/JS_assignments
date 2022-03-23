c3a403218364beb60cfb33e52b6b9762478a06a8
ankita-2021 <ankitachakrawarti@gmail.com>
2022-03-23 07:27:38 +0530

    submit_Assignment_8

--- assignments/todo_list/script_api.js
+++ assignments/todo_list/script_api.js
@@ -1,3 +1,17 @@
+let Input = document.querySelector('input');
+let btn = document.querySelector('button');
+let content = document.querySelector('.todos');
+let count =-1
+btn.addEventListener('click',function(){
+    count +=1;
+    let paragraph =document.createElement('p');
+    paragraph.innerHTML= Input.value
+    paragraph.setAttribute("key",count);
+    content.appendChild(paragraph);
+    paragraph.addEventListener('click',function(){
+        content.removeChild(paragraph);
+    })
+})
 const compBtn = document.querySelector('button#comp');
 const pendBtn = document.querySelector('button#pend');
 async function fetchtodo(){
